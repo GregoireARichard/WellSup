65fbbf352ef07b8e49421bb29e0786adbc17f891
Pierre Clerc <pierre.clerc@icloud.com>
2022-03-18 16:07:09 +0100

    update

--- app/controllers/parser_controller.rb
+++ app/controllers/parser_controller.rb
@@ -9,10 +9,29 @@ class ParserController < ApplicationController
         json["results"].each do |school|
             puts "établissement --"
             schoolsInfo = [
+                "action_de_formation_af_identifiant_onisep",
+                "formation_for_libelle",
+                "for_url_et_id_onisep",
+                "for_indexation_domaine_web_onisep",
+                "for_type",
+                "for_nature_du_certificat",
+                "for_niveau_de_sortie",
                 "lieu_denseignement_ens_libelle",
-                "ens_code_postal",
                 "ens_commune",
-                "ens_region"
+                "ens_statut",
+                "ens_adresse",
+                "ens_code_postal",
+                "ens_departement",
+                "ens_academie",
+                "ens_region",
+                "ens_n_telephone",
+                "ens_site_web",
+                "ens_hebergement",
+                "ens_accessibilite",
+                "af_modalites_scolarite",
+                "af_elements_denseignement",
+                "af_cout_scolarite",
+                "af_cout_dinscription"
             ]
             school.each do |key, value|
                 if schoolsInfo.include? key
