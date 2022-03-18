9572b5c1482beb03718225897f637fa4a1838208
kokorirko <gregoire.richard@hetic.net>
2022-03-18 15:28:48 +0100

    update pierre, unstable

--- app/controllers/parser_controller.rb
+++ app/controllers/parser_controller.rb
@@ -2,15 +2,37 @@ class ParserController < ApplicationController
     def index
         require 'json'
         require 'httparty'
+        base_url = 'https://api.opendata.onisep.fr/api/1.0/dataset/605344579a7d7/search?size=10'
         response = HTTParty.get(base_url)
         json = JSON.parse(response.body)
-        count = 0
-        
+
         json["results"].each do |school|
+            puts "établissement --"
             school.each do |key, value|
-                if key == key['lieu_denseignement_ens_libelle']
-                    puts "#{value}"
+                schoolsInfo = [
+                    "lieu_denseignement_ens_libelle",
+                    "ens_code_postal",
+                    "ens_commune",
+                    "ens_region"
+                ]
+                for i in 0..schoolsInfo.length
+                    if key = key[schoolsInfo[i]]
+                        puts "#{key} : #{value}"
+                    end
                 end
+
+                # if key == key['lieu_denseignement_ens_libelle']
+                #     puts "Établissement : #{value}"   
+                # elsif 
+                #     key == key['ens_code_postal']
+                #     puts "Code postal : #{value}"   
+                # elsif 
+                #     key == key['ens_commune']
+                #     puts "Commune : #{value}"   
+                # elsif 
+                #     key == key['ens_region']
+                #     puts "Région : #{value}" 
+                # end
             end
         end
 
