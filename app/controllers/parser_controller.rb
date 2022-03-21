3c610a73757148f82b8edf56a5513073fda6bcaf
kokorirko <gregoire.richard@hetic.net>
2022-03-21 14:21:35 +0100

    remove comments 🍆

--- app/controllers/parser_controller.rb
+++ app/controllers/parser_controller.rb
@@ -46,39 +46,7 @@ class ParserController < ApplicationController
                     af_cout_dinscription: school["af_cout_dinscription"]
     
                 })
-        end
-
-            # schoolsInfo = [
-            #     "action_de_formation_af_identifiant_onisep",
-            #     "formation_for_libelle",
-            #     "for_url_et_id_onisep",
-            #     "for_indexation_domaine_web_onisep",
-            #     "for_type",
-            #     "for_nature_du_certificat",
-            #     "for_niveau_de_sortie",
-            #     "lieu_denseignement_ens_libelle",
-            #     "ens_commune",
-            #     "ens_statut",
-            #     "ens_adresse",
-            #     "ens_code_postal",
-            #     "ens_departement",
-            #     "ens_academie",
-            #     "ens_region",
-            #     "ens_n_telephone",
-            #     "ens_site_web",
-            #     "ens_hebergement",
-            #     "ens_accessibilite",
-            #     "af_modalites_scolarite",
-            #     "af_elements_denseignement",
-            #     "af_cout_scolarite",
-            #     "af_cout_dinscription"
-            # ]
-            
-            # school.each do |key, value|
-            #     if schoolsInfo.include? key
-            #         University.setQ ({key: value})
-            #     end
-            # end
+            end
         end
     end
 end
